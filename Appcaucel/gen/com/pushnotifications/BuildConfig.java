499644c00633e3125753825c89d211cd1f9488ba
Manuel sansores <manuel.sansores@ngn-mobile.com.mx>
2014-08-07 19:05:33 -0500

    modificacion recibe notificacion push y abre una ctivity

---
+++ Appcaucel/gen/com/pushnotifications/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pushnotifications;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Appcaucel/src/com/pushnotifications/Json.java
@@ -0,0 +1,21 @@
+package com.pushnotifications;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class Json {
+	
+	public static JSONObject getJSON(String data){
+		//JSONObject jArray = null;
+		JSONObject object = null;
+		try {
+			object = (JSONObject) new JSONTokener(data).nextValue();
+		} catch (JSONException e) {
+			
+			e.printStackTrace();
+		}
+		 return object;
+	}
+
+}
