d3840e9b5a3e5216cde4385523a995ffa60ad857
Manuel sansores <manuel.sansores@ngn-mobile.com.mx>
2014-08-09 11:56:24 -0500

    cambios

---
+++ Appcaucel/src/com/pushnotifications/IntentPausa.java
@@ -0,0 +1,17 @@
+package com.pushnotifications;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class IntentPausa extends IntentService{
+	
+	public IntentPausa() {
+        super("IntentPausa");
+    }
+
+	@Override
+	protected void onHandleIntent(Intent intent) 
+	{
+		
+	}
+}
